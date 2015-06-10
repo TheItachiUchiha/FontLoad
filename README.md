9937cb4d08101dc81f78fdb4b6f289458299c469
TheItachiUchiha <abhinay_agarwal@live.com>
2015-06-10 21:49:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FontLoad
+A simple JavaFX application which shows to correctly load a font
---
+++ src/main/java/com/ita/LoadFont.java
@@ -0,0 +1,28 @@
+package com.ita;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class LoadFont extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("JavaFX Application");
+        Button button = new Button("My Button");
+        VBox box = new VBox(15, label, button);
+        box.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(box, 500, 300);
+        scene.getStylesheets().add(getClass().getResource("/fontstyle.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
